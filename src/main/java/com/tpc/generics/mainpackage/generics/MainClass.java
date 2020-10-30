926906a48b776ab2470531331c79e7f88eea7531
TipoProProger <31341005+TipoProProger@users.noreply.github.com>
2020-10-30 12:04:04 +0300

    Update MainClass.java

--- src/main/java/com/tpc/generics/mainpackage/generics/MainClass.java
+++ src/main/java/com/tpc/generics/mainpackage/generics/MainClass.java
@@ -1,19 +1,39 @@
 package com.tpc.generics.mainpackage.generics;
-
 /**
  *
  * @author michael
  */
 public class MainClass {
+    
+    private MyGenericCache cache;
+    
     public static void main(String args[]) {
-        MyGenericCache cache = new MyGenericCache();
+        MainClass mc = new MainClass();
         
-        cache.addElem("AuthorName", "Michael");
-        cache.addElem("TestNumber", 123);
-        cache.addElem(123, "Qwerty");
+        mc.cache = new MyGenericCache();
         
-        System.out.println(cache.getElem("AuthorName"));
-        System.out.println(cache.getElem("TestNumber"));
-        System.out.println(cache.getElem(123));
+        mc.cache.addElem("AuthorName", "Michael");
+        mc.cache.addElem("TestNumber", 123);
+        mc.cache.addElem(123, "Qwerty");
+        
+        System.out.println(mc.cache.getElem("AuthorName"));
+        System.out.println(mc.cache.getElem("TestNumber"));
+        System.out.println(mc.cache.getElem(123));
+        
+        System.out.println();
+        
+        mc.printMeow(mc.getValue("TestNumber"));
+        mc.printMeow(mc.getValue(123));
+        //mc.printMeow(mc.<?, ?>getValue(123));
+        //System.out.println(mc.<Integer, String>getValue(123));
+        //System.out.println(mc.<String, Integer>getValue("TestNumber"));
+    }
+    
+    public <K> Object getValue(Object key) {
+        return cache.getElem((K)key);
+    }
+    
+    public <V> void printMeow(Object obj) {
+        System.out.println((V)obj);
     }
 }
